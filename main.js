34d723e423eb369f7ce787dc6287617541529914
KitazawaSeiya <evesanta1224@yahoo.co.jp>
2017-12-31 12:46:50 +0900

    シークに合わせてチャプターの色を変更

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
 console.log(1)
 
-video_data = [[7, "停止点灯"], [12, "カウントクリア"], [15, "早送り"], [18, "カウント一致点滅"]]
+video_data = [[0, "最初から"], [7, "停止点灯"], [12, "カウントクリア"], [15, "早送り"], [18, "カウント一致点滅"]]
 
 $(function () {
   const main_video = document.getElementById("video");
 
-  //  for (let i = 0; i < video_data.length; i++) {
-  //    const li = $('<li></li>')
-  //    const in_li = $('<span></span>').text('#' + ((video_data[i][0] / 60) | 0) + ":" + ('00' + video_data[i][0] % 60).slice(-2) + "    ").addClass('seekMin')
-  //
-  //    const li_title = $('<span></span>').text(video_data[i][1]).addClass('seekTitle')
-  //
-  //    li.append(in_li).append(li_title).on('click', function () {
-  //      main_video.currentTime = video_data[i][0];
-  //    });
-  //    $('.seekList').append(li)
-  //  }
   for (let i = 0; i < video_data.length; i++) {
     tr = $("<tr></tr>")
     tr.append($("<td></td>").text(((video_data[i][0] / 60) | 0) + ":" + ('00' + video_data[i][0] % 60).slice(-2)))
@@ -26,16 +15,19 @@ $(function () {
     $(".chapterList").append(tr);
   }
 
-
-  stop = $('<li></li>').text('stop').on('click', function () {
-    main_video.pause();
-  });
-
-  seek = $('<li></li>').text('#0:12').on('click', function () {
-    main_video.currentTime = 14;
-  });
-
-
+  setInterval(function () {
+    for (let i = 0; i < video_data.length; i++) {
+      $("tbody>tr:nth-child(" + (i + 2) + ")").removeClass('nowPlay')
+      if (video_data[i][0] <= main_video.currentTime && ((i + 1) == video_data.length || main_video.currentTime < video_data[i + 1][0])) {
+        $("tbody>tr:nth-child(" + (i + 2) + ")").addClass('nowPlay')
+      }
+    }
+    //console.log(main_video.currentTime)
+  }, 200);
   //$('.seekList').append(seek)
 
+  //  setInterval(function () {
+  //    console.log(main_video.currentTime)
+  //  }, 1000);
+  //  $("tr:nth-child(2)").addClass('nowPlay')
 });
